1b2d2d748d36a4f17a352f57a254a8be722eb18d
acedly <acedly.app@gmail.com>
2025-07-03 12:34:31 +0100

    Updated navbar.tsx

--- src/components/home/navigation/navbar.tsx
+++ src/components/home/navigation/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                     </div>
                     <nav className="hidden md:block absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
                         <ul className="flex items-center justify-center gap-8">
-                            <Link to="#" className="hover:text-foreground/80 text-sm">Pricing</Link>
+                            <Link to="#" className="hover:text-foreground/80 text-sm">About</Link>
                             <Link to="#" className="hover:text-foreground/80 text-sm">About</Link>
                             <Link to="#" className="hover:text-foreground/80 text-sm">Features</Link>
                             <Link to="#" className="hover:text-foreground/80 text-sm">Blog</Link>
