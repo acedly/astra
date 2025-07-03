462a0227e96f1628aa9c51f549681e6243921046
acedly <acedly.app@gmail.com>
2025-07-03 13:29:27 +0100

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -40,7 +40,7 @@ const HomePage = () => {
 
                             <div className="flex flex-col items-center mt-8 max-w-3xl w-11/12 md:w-full">
                                 <h1 className="text-4xl md:text-6xl lg:textxl md:!leading-snug font-semibold text-center bg-clip-text bg-gradient-to-b from-gray-50 to-gray-50 text-white">
-                                    CBT tests, study packs, and AI tools. All in one sleek app
+                                    Study smarter. Score higher
                                 </h1>
                                 <p className="text-base md:text-lg text-foreground/80 mt-6 text-center text-white">
                                     CBT tests, study packs, and AI tools. All in one sleek app. A fast, smart vault for exam prep: past questions, CBTs, study packs, and minimal AI tools.
