e4b05d21148dc0f960a3b603552e1b10c1966f2f
acedly <acedly.app@gmail.com>
2025-07-03 13:26:06 +0100

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -33,14 +33,14 @@ const HomePage = () => {
                                 <span className="h-full w-full blur-md absolute bottom-0 inset-x-0 bg-gradient-to-tr from-primary/40"></span>
                                 <span className="z-10 py-0.5 text-sm text-neutral-100 flex items-center justify-center gap-1.5">
                                     <img src="/icons/sparkles-dark.svg" alt="✨" width={24} height={24} className="w-4 h-4" />
-                                    Introducing Astra AI
+                                    Introducing Kluis
                                     <ChevronRight className="w-4 h-4" />
                                 </span>
                             </button>
 
                             <div className="flex flex-col items-center mt-8 max-w-3xl w-11/12 md:w-full">
                                 <h1 className="text-4xl md:text-6xl lg:textxl md:!leading-snug font-semibold text-center bg-clip-text bg-gradient-to-b from-gray-50 to-gray-50 text-white">
-                                    Build your next idea and ship your dream site
+                                    Kluis — Your Exam Vault
                                 </h1>
                                 <p className="text-base md:text-lg text-foreground/80 mt-6 text-center text-white">
                                     Zero code, maximum speed. Make professional sites easy, fast and fun while delivering best-in-class SEO, performance.
