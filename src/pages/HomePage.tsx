281ddaa55b966f50dabb43b35fc4921e821eec2a
acedly <acedly.app@gmail.com>
2025-07-03 12:49:56 +0100

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -47,7 +47,7 @@ const HomePage = () => {
                                 </p>
                                 <div className="hidden md:flex relative items-center justify-center mt-8 md:mt-12 w-full">
                                     <Link to="#" className="flex items-center justify-center w-max rounded-full border-t border-foreground/30 bg-white/20 backdrop-blur-lg px-2 py-1 md:py-2 gap-2 md:gap-8 shadow-3xl shadow-background/40 cursor-pointer select-none">
-                                        <p className="text-foreground text-sm text-center md:text-base font-medium pl-4 pr-4 lg:pr-0">
+                                        <p className="text-foreground text-sm text-center md:text-base font-medium pl-4 pr-4 lg:pr-0 text-white">
                                             ✨ {"  "} Start building your dream website now!
                                         </p>
                                         <Button size="sm" className="rounded-full hidden lg:flex border border-foreground/20">
